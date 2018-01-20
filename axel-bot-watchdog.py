8565742e20aaf09c5a08e3ad233280c428aaa8f1
Matthew Lancellotti <mareoraft@gmail.com>
2018-01-20 17:33:07 -0500

    made python path a little more airtight.  troubleshooting.

--- axel-bot-watchdog.py
+++ axel-bot-watchdog.py
@@ -10,10 +10,11 @@ from watchdog.events import FileSystemEventHandler
 
 # config
 DIR = os.path.dirname(os.path.abspath(__file__))
+PARENT_DIR = os.path.dirname(DIR)
 UPSTREAM_REPO_NAME = "Integrable-Probability-Working-Seminar"
 LOCAL_REPO_NAME = "axel-bot-{}".format(UPSTREAM_REPO_NAME)
-LOCAL_REPO_PATH="{}/../{}".format(DIR, LOCAL_REPO_NAME)
-
+LOCAL_REPO_PATH=os.path.join(PARENT_DIR, LOCAL_REPO_NAME)
+print(LOCAL_REPO_PATH)
 
 class FileUpdateEventHandler (FileSystemEventHandler):
 
@@ -28,9 +29,11 @@ class FileUpdateEventHandler (FileSystemEventHandler):
 
     def on_created_or_modified(self, event):
         print('The event is: "' + str(event) + '".')
+        print(event.src_path)
         if re.search(r'website\.md$', event.src_path):
             print('Found a new or modified file of interest.  Executing axel-bot...')
             try:
+                print('about to subprocess.')
                 print(subprocess.check_output('axel-bot.sh ' + str(event.src_path), shell=True))
             except Exception as e:
                 print(e)
@@ -41,6 +44,7 @@ if __name__ == "__main__":
                         datefmt='%Y-%m-%d %H:%M:%S')
     event_handler = FileUpdateEventHandler()
     observer = Observer()
+    print('I start watch')
     observer.schedule(event_handler, LOCAL_REPO_PATH, recursive=True)
     observer.start()
     try:
