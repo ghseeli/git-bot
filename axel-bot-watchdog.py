2df70eb5117853ca758014a67baf58a4327af3b5
Matthew Lancellotti <mareoraft@gmail.com>
2018-01-20 17:25:12 -0500

    a python watchdog to run axel-bot whenever it sees the file changed

---
+++ axel-bot-watchdog.py
@@ -0,0 +1,51 @@
+import sys
+import os
+import time
+import logging
+import re
+import subprocess
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+# config
+DIR = os.path.dirname(os.path.abspath(__file__))
+UPSTREAM_REPO_NAME = "Integrable-Probability-Working-Seminar"
+LOCAL_REPO_NAME = "axel-bot-{}".format(UPSTREAM_REPO_NAME)
+LOCAL_REPO_PATH="{}/../{}".format(DIR, LOCAL_REPO_NAME)
+
+
+class FileUpdateEventHandler (FileSystemEventHandler):
+
+
+    def on_created(self, event):
+        print('A file or directory has been created.')
+        return self.on_created_or_modified(event)
+
+    def on_modified(self, event):
+        print('A file or directory has been modified.')
+        return self.on_created_or_modified(event)
+
+    def on_created_or_modified(self, event):
+        print('The event is: "' + str(event) + '".')
+        if re.search(r'website\.md$', event.src_path):
+            print('Found a new or modified file of interest.  Executing axel-bot...')
+            try:
+                print(subprocess.check_output('axel-bot.sh ' + str(event.src_path), shell=True))
+            except Exception as e:
+                print(e)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    event_handler = FileUpdateEventHandler()
+    observer = Observer()
+    observer.schedule(event_handler, LOCAL_REPO_PATH, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(0.5)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
