56652a713d23e9bef6c6ad7d8f554958d3400051
Matthew Lancellotti <mareoraft@gmail.com>
2018-01-20 18:59:46 -0500

    remove unwanted print statements in watchdog, and alter some

--- axel-bot-watchdog.py
+++ axel-bot-watchdog.py
@@ -26,24 +26,21 @@ class FileUpdateEventHandler (FileSystemEventHandler):
 
     def on_created_or_modified(self, event):
         print('The event is: "' + str(event) + '".')
-        print(event.src_path)
-        print(type(event.src_path))
         if event.src_path == FILE_PATH_TO_WATCH:
-            print('Found a new or modified file of interest.  Executing axel-bot...')
+            print('The file of interest has been altered!  Executing axel-bot...')
             try:
-                print('about to subprocess.')
                 output = subprocess.check_output('./axel-bot.sh', shell=True)
                 print(output)
             except Exception as e:
                 print(e)
 
 if __name__ == "__main__":
+    print('Watching for alterations...')
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
     event_handler = FileUpdateEventHandler()
     observer = Observer()
-    print('I start watch')
     observer.schedule(event_handler, DIR_PATH_TO_WATCH, recursive=True)
     observer.start()
     try:
